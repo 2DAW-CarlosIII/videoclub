6106a3baa6ab5ea8a14d4af073c0e1c5baaac24b
8686469 <8686469@alu.murciaeduca.es>
2021-01-31 21:40:14 +0100

    Autenticación API en videoclub

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\PeliculaController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,10 +16,11 @@ use App\Http\Controllers\API\PeliculaController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('peliculas', PeliculaController::class);
-
+Route::middleware('auth:sanctum')->apiResource('peliculas', PeliculaController::class);
+Route::post('tokens/create',[LoginController::class,'createToken']);
 Route::get('peliculas/search/{search}', [PeliculaController::class, 'search'])->name('peliculas.search');
+
